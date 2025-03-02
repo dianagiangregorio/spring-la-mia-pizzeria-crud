10e6e5b19dd25c891308b31483ce1ed8c691b2bd
dianagiangregorio <diana.giangregorio@gmail.com>
2025-03-02 17:44:22 +0100

    add: sping crud project

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,66 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "pizze")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Lob
+    private String descrizione;
+
+    // @NotBlank
+    // private String fotoUrl;
+
+    @NotBlank
+    private float prezzo;
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    // public String fotoUrl() {
+    //     return this.fotoUrl;
+    // }
+
+    // public void setFotoUrl(String fotoUrl) {
+    //     this.fotoUrl = fotoUrl;
+    // }
+
+    public float getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    
+}
